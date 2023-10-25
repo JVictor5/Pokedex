2ced93d607f6b5de982ef476089ecf047650263d
JVictor5 <josevictor_caravantealves10@hotmail.com>
2023-10-25 04:26:26 -0300

    type

--- src/js/pokeScript.js
+++ src/js/pokeScript.js
@@ -1,6 +1,7 @@
 const pokemonNome = document.querySelector(".pokemonNome");
 const pokemonNum = document.querySelector(".pokemonNum");
 const pokemonIMG = document.querySelector(".pokemonIMG");
+const pokemonType = document.querySelector(".pokemonTipos");
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
@@ -14,12 +15,29 @@ const renderPokemon = async (pokemon) => {
   const data = await fetchPokemon(pokemon);
   pokemonNome.innerHTML = data.name;
   pokemonNum.innerHTML = data.id;
-  pokemonIMG.src = data["sprites"]["front_default"]; /* pokemon animado */
 
-  /* pokemonImage.src =
-     data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
-       "front_default"
-     ]; */
+  pokemonIMG.src =
+    data["sprites"]["other"]["official-artwork"][
+      "front_default"
+    ]; /* pokemon com a arte official */
+  // const types = data.types.map((typeSlot) => typeSlot.type.name).join("  ");
+  // pokemonType.innerHTML = types;
+  // const tipo = data.types[1].type.name;
+  // pokemonType.innerHTML = `Tipo: <span class="tipo-${tipo} tipo-${tipo}">${tipo}${tipo}</span>`;
+
+  const tipos = data.types.map((typeSlot) => typeSlot.type.name);
+
+  tipos.forEach((tipo) => {
+    const tipoElement = document.createElement("span");
+    tipoElement.textContent = tipo;
+    tipoElement.classList.add("tipo-" + tipo);
+    pokemonType.appendChild(tipoElement);
+  });
 };
 
-renderPokemon("1010");
+renderPokemon("6");
+
+// pokemonIMG.src =
+//   data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+//     "front_default"
+//   ];
